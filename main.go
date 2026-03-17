2251bd18b5a7d4a21009622b193757c7621ee3af
Raj Patel <83405614+rajpatelbot@users.noreply.github.com>
2026-03-18 03:24:46 +0530

    feat: Implemented feature to get recent clipboard content

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "os/exec"
+
+func pasteFromClipboard() (content string, err error) {
+	cmd := exec.Command("pbpaste")
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
 func main() {
 	go watcher()
 	startHotkey()
